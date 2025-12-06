2896350b95a201d13b6c5d7194db3db16669d505
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-12-07 01:35:29 +0600

    implement final commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,  // Log the value of MONGODB_URI
+  },
+};
+
+console.log("MONGODB_URI in next.config.mjs:", process.env.MONGODB_URI);
 
 export default nextConfig;
